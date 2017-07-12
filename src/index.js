1ee28b4f8e90702098098d21d8075a11b0e3f4ae
Cardumen <cardumen@MacBook-Pro-de-Luis-Vilches.local>
2017-07-12 18:27:48 -0400

    advance system routes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
 import Sempli, {ORM} from './sempli.js';
 
-const DB = new ORM();
+const DB = new ORM({
+    database: 'blog',
+    user: 'blog',
+    pass: 'blog',
+    host: 'ds151461.mlab.com:51461',
+    engine: 'mongodb'
+});
+
+export default DB;
 
 Sempli.createServer(3000);
 
-
-
-Sempli.point({
-    method: 'GET',
-    url: '/all',
-    handler: function(req,res){
-        DB.conexion('hola');
+var route = [
+    {
+        url: '/',
+        method: 'GET',
+        handler: function (req, res) {
+            res.send('hello desde le home!')
+        }
+    },
+    {
+        url: '/all',
+        method: 'GET',
+        handler: function (req, res) {
+            res.send('hello desde all!')
+        }
+    },
+    {
+        url: '/dos',
+        method: 'GET',
+        handler: function (req, res) {
+            res.send('hello! desde dos')
+        }
     }
-});
+]
 
-Sempli.point({
-    method: 'GET',
-    url: '/add',
-    handler: function(req,res){
-
-    }
-});
+Sempli.routes(route);
--- src/sempli.js
+++ src/sempli.js
@@ -2,19 +2,21 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import chalk from 'chalk';
+import mongoose from 'mongoose';
 
 class Sempli{
     constructor(){
         this.app = express();
         this.cors = cors;
         this.body = bodyParser;
+        this.arr = [];
         this.config();
     }
 
     createServer(port){
         this.app.listen(port, err => {
             if(err){
-                console.log('Error:','->',err);
+                console.log('-> Error:','=>',err);
             }else{
                 console.log(chalk.magenta('-> Sempli server running in port'),chalk.cyan(port));
             }
@@ -34,42 +36,75 @@ class Sempli{
         return this.app.set(args);
     }
 
-    point(obj){
-        this.method = obj.method;
-        this.url = obj.url;
-        this.handler = obj.handler;
+    register(obj){
+        this.arr.push(obj);
+    }
 
-        switch (this.method){
-            case null || 'undefined':
-                return this.app.get(this.url,this.handler);
-                break;
-            case 'GET' || 'get':
-                return this.app.get(this.url,this.handler);
-                break;
-            case ('POST' || 'post'):
-                return this.app.post(this.url,this.handler);
-                break;
-            case ('PUT' || 'put'):
-                return this.app.put(this.url,this.handler);
-                break;
-            case ('DELETE' || 'delete'):
-                return this.app.delete(this.url,this.handler);
-                break;
-            default:
-                console.log(chalk.red('* Method error => ', this.method , '->'), chalk.blue('url['+ '"'+this.url+'"]'));
-        }
+    routes(routes){
+
+        var app = this.app;
+        routes.map(function(i){
+            switch (i.method){
+                case null || 'undefined':
+                    return app.get(i.url,i.handler);
+                    break;
+                case 'GET' || 'get':
+                    return app.get(i.url,i.handler);
+                    break;
+                case ('POST' || 'post'):
+                    return app.post(i.url,i.handler);
+                    break;
+                case ('PUT' || 'put'):
+                    return app.put(i.url,i.handler);
+                    break;
+                case ('DELETE' || 'delete'):
+                    return app.delete(i.url,i.handler);
+                    break;
+                default:
+                    console.log(chalk.red('* Method error => ', this.method , '->'), chalk.blue('url['+ '"'+this.url+'"]'));
+            }
+        })
     }
 }
 
-export class ORM extends Sempli {
-    constructor(name){
-        super();
-        this.name = name;
+export class ORM {
+    constructor(o){
+        this.db = o;
+        this.conect();
     }
 
-    conexion(msg){
-        console.log(chalk.white(msg));
+    conect(){
+        switch (this.db.engine){
+            case"mongodb":
+                this.cMongo(this.db.engine);
+                break;
+
+            default:
+                console.log(chalk.red('-> Engine database not support'));
+        }
     }
+
+    cMongo(){
+        let user = this.db.user;
+        let pass = this.db.pass;
+        let host = this.db.host;
+        let db = this.db.database;
+
+        mongoose.connect(`mongodb://${user}:${pass}@${host}/${db}`, err => {
+            if(err) {
+                console.log(chalk.red('-> Error en la coneccion con la base de datos =>', err));
+            } else {
+              console.log(chalk.magenta('-> Conección exitosa con la base de datos =>'), chalk.cyan(db));
+            }
+        });
+    }
+
+    cMysql(){}
+    cPg(){}
+    cSqlite(){}
+
+    model(){}
+    save(){}
 }
 
 const app = new Sempli();
